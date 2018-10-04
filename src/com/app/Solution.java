da990b1e18bef418ebab8ea155c28d664a648eb2
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2018-10-04 11:45:06 +0300

    Fix the 3x3 test case

--- src/com/app/Solution.java
+++ src/com/app/Solution.java
@@ -15,10 +15,10 @@ public class Solution {
         final int N = matrix.length;
         for (int i = 0; i < N - 1; i++) {
             int t = matrix[0][i];
-            matrix[0][i] = matrix[N - 1][0];
-            matrix[N - 1][0] = matrix[N - 1][N - 1];
-            matrix[N - 1][N - 1] = matrix[0][N - 1];
-            matrix[0][N - 1] = t;
+            matrix[0][i] = matrix[N - 1 - i][0];
+            matrix[N - 1 - i][0] = matrix[N - 1][N - 1 - i];
+            matrix[N - 1][N - 1 - i] = matrix[i][N - 1];
+            matrix[i][N - 1] = t;
         }
     }
 }
